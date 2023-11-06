c4a763ad0c96a981a1a5b93040ca2a15d662753e
Alex <alexanderls212@gmail.com>
2023-11-06 15:31:44 -0600

    General Styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import toDoLogo from './images/to-do-logo.png';
+import Tarea from './components/Task';
 
 function App() {
   return (
-    <div className="App">
-      React
+    <div className="to-do-app">
+      <div className='logo-container'>
+        <img 
+          src={toDoLogo} 
+          className='app-logo'
+        />
+      </div>
+      <div className='principal-list'>
+        <h1>My Tasks</h1>
+        <Tarea text='Learn React'/>
+      </div>
     </div>
   );
 }
