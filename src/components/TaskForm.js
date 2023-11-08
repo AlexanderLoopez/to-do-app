dd8bc67894af98d8007171c0cd256a54518f5685
Alex <alexanderls212@gmail.com>
2023-11-08 17:43:41 -0600

    Finish, check erros

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,23 +1,36 @@
 import React, { useState } from 'react';
 import '../style sheets/TaskForm.css';
+import { v4 as uuidv4 } from 'uuid';
 
 function TaskForm(props) {
 
   const [input, setInput] = useState('');
 
   const manageChange = e => {
-    console.log('Writing...')
+    setInput(e.target.value);
   }
 
   const manageShipping = e => {
+
+    /*Preventing the page from reloading */
+    
+    e.preventDefault();
+
+    /*Giving the task an id*/
+    
     const newTask = {
-      id: '34545',
-      text: 'Hi'
-    }
-  }
+      id: uuidv4(),
+      text: input,
+      completed: false
+    };
+
+    props.onSubmit(newTask);
+  };
 
   return (
-    <form className='task-form'>
+    <form 
+    className='task-form'
+    onSubmit={manageShipping}>
       <input 
         className='task-input'
         type='text'
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -8,22 +8,45 @@ function TaskList() {
   const [tasks, setTask] = useState([]);
 
   const addTask = task => {
-    console.log('Tarea agregada');
-    console.log(task);
-  }
+    if (task.text.trim()) {
+      task.text = task.text.trim();
+
+      const updatedTasks = [task, ...tasks];
+      setTask(updatedTasks);
+    };
+  };
+
+  const deleteTask = id => {
+    const updatedTasks = tasks.filter(task => task.id !== id);
+    setTask(updatedTasks);
+  };
+
+  const completeTask = id => {
+    const updatedTasks = tasks.map(task => {
+      if (task.id === id) {
+        task.completed = !task.completed;
+      }
+      return task;
+    });
+    setTask(updatedTasks);
+  };
 
   return (
     <>
-      <TaskForm />
+      <TaskForm onSubmit={addTask}/>
       <div className='task-list-container'>
         {
           tasks.map((task) =>
             <Task 
+              key={task.id}
+              id={task.id}
               text={task.text}
               completed={task.completed}
+              completeTask={completeTask}
+              deleteTask={deleteTask} 
             />
           )
-        }
+        };
       </div>
     </>
   );
